bc46cca8c2f62375f1a6bcc97bdd8141df573b90
Matthew Kataryniak <mattzapp17@gmail.com>
2018-10-27 00:36:25 -0400

    Updated CSS with sinnoh support

--- scripts/main.js
+++ scripts/main.js
@@ -33,7 +33,92 @@ $(function () {
     289, 295, 308, 310, 321, 330, 334, 350, 373,
     376,
     // Unreleased Hoenn
-    290, 291, 292, 352, 366, 367, 368
+    290, 291, 292, 352, 366, 367, 368,
+	// SINNOH
+  389, // Torterra
+  392, // Infernape
+  395, // Empoleon
+  404, // Luxio
+  405, // Luxray
+  406, // Budew
+  407, // Roserade
+  408, // Cranidos
+  409, // Rampardos
+  410, // Shieldon
+  411, // Bastiodon
+  412, // Burmy
+  413, // Wormadam
+  414, // Mothim
+  415, // Combee
+  416, // Vespiquen
+  418, // Buizel
+  419, // Floatzel
+  420, // Cherubi
+  421, // Cherrim
+  422, // Shellos
+  423, // Gastrodon
+  424, // Ambipom
+  426, // Drifblim
+  429, // Mismagius
+  430, // Honchkrow
+  431, // Glameow
+  432, // Purugly
+  433, // Chingling
+  435, // Skuntank
+  436, // Bronzor
+  437, // Bronzong
+  438, // Bonsly
+  439, // Mime Jr.
+  440, // Happiny
+  443, // Gible
+  444, // Gabite
+  445, // Garchomp
+  446, // Munchlax
+  447, // Riolu
+  448, // Lucario
+  449, // Hippopotas
+  450, // Hippowdon
+  451, // Skorupi
+  452, // Drapion
+  453, // Croagunk
+  454, // Toxicroak
+  456, // Finneon
+  457, // Lumineon
+  458, // Mantyke
+  459, // Snover
+  460, // Abomasnow
+  461, // Weavile
+  462, // Magnezone
+  463, // Lickilicky
+  464, // Rhyperior
+  465, // Tangrowth
+  466, // Electivire
+  467, // Magmortar
+  468, // Togekiss
+  469, // Yanmega
+  470, // Leafeon
+  471, // Glaceon
+  472, // Gliscor
+  473, // Mamoswine
+  474, // Porygon-Z
+  475, // Gallade
+  476, // Probopass
+  477, // Dusknoir
+  478, // Froslass
+  479, // Rotom
+  480, // Uxie
+  481, // Mesprit
+  482, // Azelf
+  483, // Dialga
+  484, // Palkia
+  485, // Heatran
+  486, // Regigigas
+  488, // Cresselia
+  489, // Phione
+  490, // Manaphy
+  491, // Darkrai
+  492, // Shaymin
+  493 // Arceus
   ];
 
   $.get('snaps/snaps.json')
